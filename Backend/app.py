4d46d94936f403a1b580f4091440f49c8ba640de
unknown <lukotas100@gmail.com>
2025-07-31 22:30:36 -0300

    Add actualizacion de las tablas y se añadieron los servicios para obtener los clientes y empleados

--- Backend/app.py
+++ Backend/app.py
@@ -21,14 +21,26 @@ class Empleado(db.Model):
     last_name = db.Column(db.String(100),  nullable=False)
     type = db.Column(db.String(50), nullable=False) 
 
+class Cliente(db.Model):
+    __tablename__ = 'cliente'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    phone_number = db.Column(db.String(20), nullable=True)
+    genre = db.Column(db.String(20), nullable=True)
+
 # Creacion de la base datos y usuarios
 # with app.app_context():
 #     db.create_all()
 
 #     admin_user = Empleado(email = "lucasabalos531@gmail.com", password_hashed = generate_password_hash("admin123", method="pbkdf2:sha256"), name = "Lucas", last_name = "Abalos", type = "user_admin")
 #     empleado_user = Empleado(email = "joseperez@gmail.com", password_hashed = generate_password_hash("empleado123", method="pbkdf2:sha256"), name = "Jose", last_name = "Perez", type = "user_empleado")
+#     cliente = Cliente(name = "Martin", last_name = "Gonzales", phone_number = "1123212354", genre="Masculino")
+    
 #     db.session.add(admin_user)
 #     db.session.add(empleado_user)
+#     db.session.add(cliente)
 #     db.session.commit()
 #     print("base de datos y usuario creados correctamente")
 
@@ -46,8 +58,35 @@ def login():
         return jsonify({"success": False, "message": "La contraseña es incorrecta"}), 401
     elif user and check_password_hash(user.password_hashed, user_password):
         return jsonify({"success":True, "usuario": f"{user.name} {user.last_name}", "rol": user.type}),200
-    
 
+
+@app.route("/getWorkers", methods=['GET'])
+def get_workers():
+    empleados = Empleado.query.all()
+    
+    if not empleados:
+        return jsonify({"success": False, "message": "No hay empleados en la base de datos"}),404
+
+    workers = []
+    
+    for emp in empleados:
+        workers.append({"id": emp.id, "correo": emp.email ,"nombre": emp.name, "apellido": emp.last_name, "rol": emp.type})
+    
+    return jsonify({"success": True, "data": workers}),200
+
+@app.route("/getClients", methods=['GET'])
+def get_clients():
+    clientes = Cliente.query.all()
+
+    if not clientes:
+        return jsonify({"success": False, "message": "No se encontro ningun cliente en la base de datos"}), 404
+
+    clients = []
+
+    for client in clientes:
+        clients.append({"id": client.id, "nombre": client.name, "apellido": client.last_name, "telefono": client.phone_number, "genero": client.genre})
+           
+    return jsonify({"success": True, "data": clients}),200
 
 @app.route("/")
 def index():
