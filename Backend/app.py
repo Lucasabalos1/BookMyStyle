380e99beea756ffd0272a681b555d6ce0b8d688f
unknown <lukotas100@gmail.com>
2025-08-05 20:00:03 -0300

    add CRUD para empleados y clientes

--- Backend/app.py
+++ Backend/app.py
@@ -27,7 +27,7 @@ class Cliente(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
-    phone_number = db.Column(db.String(20), nullable=True)
+    phone_number = db.Column(db.String(20), nullable=True, unique=True)
     genre = db.Column(db.String(20), nullable=True)
 
 # Creacion de la base datos y usuarios
@@ -60,6 +60,7 @@ def login():
         return jsonify({"success":True, "usuario": f"{user.name} {user.last_name}", "rol": user.type}),200
 
 
+#Endpoints para los empleados
 @app.route("/getWorkers", methods=['GET'])
 def get_workers():
     empleados = Empleado.query.all()
@@ -74,6 +75,74 @@ def get_workers():
     
     return jsonify({"success": True, "data": workers}),200
 
+@app.route("/addEmpleado", methods=['POST'])
+def add_empleado():
+    data = request.get_json()
+    email = data.get("email")
+    password = data.get("password")
+    name = data.get("nombre")
+    last_name = data.get("apellido")
+    type = data.get("rol")
+    
+    if not email or not password or not name or not last_name or not type :
+        return jsonify({"success": False, "message": "Los campos no pueden quedar vacios"}), 400
+    
+    user_exists = Empleado.query.filter_by(email = email).first()
+
+    if user_exists:
+        return jsonify({"success": False, "message": "El empleado ya existe"}), 409
+    
+    new_worker = Empleado(email = email, password_hashed = generate_password_hash(password, method="pbkdf2:sha256"), name = name, last_name = last_name, type = type)
+    db.session.add(new_worker)
+    db.session.commit()
+   
+    return jsonify({"success": True, "message": "El empleado se registro correctamente"}), 201
+
+@app.route("/editarEmpleado/<int:id>", methods=['PUT'])
+def editar_empleado(id):
+    data = request.get_json()
+    new_email = data.get("email")
+    new_password = data.get("password")
+    new_name = data.get("nombre")
+    new_last_name = data.get("apellido")
+    new_type = data.get("rol")
+    
+    if not new_email or not new_password or not new_name or not new_last_name or not new_type :
+        return jsonify({"success": False, "message": "Los campos no pueden quedar vacios"}), 400
+    
+    existing_user = Empleado.query.filter_by(email=new_email).first()
+    if existing_user and existing_user.id != id:
+        return jsonify({"success": False, "message": "Ya existe un empleado con ese email"}), 409
+    
+    empleado = Empleado.query.get(id)
+    
+    if not empleado:
+        return jsonify({"success": False, "message": "El empleado no existe, por lo tanto no se puede editar"}),409
+    
+    empleado.email = new_email
+    empleado.password = generate_password_hash(new_password, method="pbkdf2:sha256")
+    empleado.name = new_name
+    empleado.last_name = new_last_name
+    empleado.type = new_type
+    
+    db.session.commit()
+
+    return jsonify({"success": True, "message": "El empleado fue editado correctamente"}),200
+
+
+@app.route("/eliminarEmpleado/<int:id>", methods=['DELETE'])
+def eliminar_empleado(id):    
+    empleado = Empleado.query.get(id)
+    
+    if not empleado:
+        return jsonify({"success": False, "message": "El empleado no existe, por lo tanto no se puede eliminar"}),409
+    
+    db.session.delete(empleado)
+    db.session.commit()
+    
+    return jsonify({"success": True, "message": "El empleado fue eliminado correctamente"}),200
+
+
 @app.route("/getClients", methods=['GET'])
 def get_clients():
     clientes = Cliente.query.all()
@@ -87,6 +156,66 @@ def get_clients():
         clients.append({"id": client.id, "nombre": client.name, "apellido": client.last_name, "telefono": client.phone_number, "genero": client.genre})
            
     return jsonify({"success": True, "data": clients}),200
+
+@app.route("/addClient", methods=['POST'])
+def add_client():
+    data = request.get_json()
+    name = data.get("nombre")
+    last_name = data.get("apellido")
+    phone_number = data.get("telefono")
+    genre = data.get("genero")
+    
+    if not name or not last_name or not phone_number or not genre:
+        return jsonify({"success": False, "message": "Los campos no pueden quedar vacios"}), 400
+    
+    client_exists = Cliente.query.filter_by( phone_number = phone_number).first()
+    
+    if client_exists:
+        return jsonify({"success": False, "message": "El cliente ya se encuentra registrado"}), 409
+
+    new_client = Cliente(name = name, last_name = last_name, phone_number = phone_number, genre = genre)
+    db.session.add(new_client)
+    db.session.commit()
+    
+    return jsonify({"success": True, "message": "El cliente se registro correctamente"}), 201
+
+@app.route("/editarCliente/<int:id>", methods=['PUT'])
+def editar_cliente(id):
+    data = request.get_json()
+    new_name = data.get("nombre")
+    new_last_name = data.get("apellido")
+    new_phone_number = data.get("telefono")
+    new_genre = data.get("genero")
+    
+    if not new_name or not new_last_name or not new_phone_number or not new_genre:
+        return jsonify({"success": False, "message": "Los campos no pueden quedar vacios"}), 400
+    
+    client = Cliente.query.get(id)
+    
+    if not client:
+        return jsonify({"success": False, "message": "El cliente no existe, por lo tanto no se puede editar"}),409
+    
+    client.name = new_name
+    client.last_name = new_last_name
+    client.phone_number = new_phone_number
+    client.genre = new_genre
+    
+    db.session.commit()
+
+    return jsonify({"success": True, "message": "El cliente fue editado correctamente"}),200
+
+
+@app.route("/eliminarCliente/<int:id>", methods=['DELETE'])
+def eliminar_cliente(id):    
+    client = Cliente.query.get(id)
+    
+    if not client:
+        return jsonify({"success": False, "message": "El cliente no existe, por lo tanto no se puede eliminar"}),409
+    
+    db.session.delete(client)
+    db.session.commit()
+    
+    return jsonify({"success": True, "message": "El cliente fue eliminado correctamente"}),200
 
 @app.route("/")
 def index():
