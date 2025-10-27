a476a193089984df2ead9316014c342164cbeeb7
unknown <lukotas100@gmail.com>
2025-10-26 22:59:24 -0300

    Fix error visual en las cards de configuracion de peluqueria

--- FrontEnd/src/Components/WorkMangar/DayCard.jsx
+++ FrontEnd/src/Components/WorkMangar/DayCard.jsx
@@ -10,23 +10,25 @@ export const DayCard = ({ day, state, desde, hasta, onToggle, onTimeChange }) =>
             <span className={styles.day}>{day}</span>
         </div>
 
-        <div className={styles.inputsContainer}>
-            <div className={styles.inputBlock}>
-                <label htmlFor={`Desde-${day}`}>Desde:</label>
-                <input type="time" name="Desde" id={`Desde-${day}`} disabled={!state} value={desde} onChange={e => onTimeChange("Desde", e.target.value)}/>
+        <div className={styles.controlersContainer}>
+            <div className={styles.inputsContainer}>
+                <div className={styles.inputBlock}>
+                    <label htmlFor={`Desde-${day}`}>Desde:</label>
+                    <input type="time" name="Desde" id={`Desde-${day}`} disabled={!state} value={desde} onChange={e => onTimeChange("Desde", e.target.value)}/>
+                </div>
+                <div className={styles.inputBlock}>
+                    <label htmlFor={`Hasta-${day}`}>Hasta:</label>
+                    <input type="time" name="Hasta" id={`Hasta-${day}`} disabled={!state} value={hasta} onChange={e => onTimeChange("Hasta", e.target.value)}/>
+                </div>
             </div>
-            <div className={styles.inputBlock}>
-                <label htmlFor={`Hasta-${day}`}>Hasta:</label>
-                <input type="time" name="Hasta" id={`Hasta-${day}`} disabled={!state} value={hasta} onChange={e => onTimeChange("Hasta", e.target.value)}/>
-            </div>
-        </div>
 
-        <div className={styles.stateContainer}>
-            <span className={`${styles.state} ${state ? styles.open : styles.closed}`}>
-                {state ? 'Abierto' : 'Cerrado'}
-            </span>
-            <input type="checkbox" className={styles.checkbox} checked={state} onChange={onToggle}/>
-            <span className={styles.slider} onClick={onToggle}></span>
+            <div className={styles.stateContainer}>
+                <span className={`${styles.state} ${state ? styles.open : styles.closed}`}>
+                    {state ? 'Abierto' : 'Cerrado'}
+                </span>
+                <input type="checkbox" className={styles.checkbox} checked={state} onChange={onToggle}/>
+                <span className={styles.slider} onClick={onToggle}></span>
+            </div>
         </div>
     </div>
     </>
